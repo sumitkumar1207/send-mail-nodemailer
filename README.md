3255bdf11e7a9b7149cbd15fe022eaeb88eec55a
sumitkumar1207 <logsumit28@gmail.com>
2018-10-04 18:45:07 +0530

    Successfully send mail with the help of nodemailer

---
+++ README.md
@@ -0,0 +1,15 @@
+step 1: Download or clone the project.
+
+step 2: npm install.
+
+step 3: npm start.
+
+There are few things that you should fix before send mail.
+1:- create reusable transporter object using the default SMTP transport
+<img src="./snap/1.JPG" width="500">
+
+2:- setup email data with unicode symbols
+<img src="./snap/2.JPG" width="500">
+
+3:- send mail with defined transport object
+<img src="./snap/3.JPG" width="500">
--- app.js
+++ app.js
@@ -34,5 +34,38 @@ app.post("/send", (req, res) => {
     <h3>Message</h3>
     <p>${req.body.message}</p>
   `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email", //What ever protocol you wish to use.
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "test@test.com", // your email
+      pass: "test123" // your password
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: "<youremailid@xyz.com>", // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.redirect("url", status()); //redirect url and status.
+    // OR
+    res.send({ msg: "Email sent or whatever msg you want" });
+  });
 });
 app.listen(3000, () => console.log("Server Started"));
